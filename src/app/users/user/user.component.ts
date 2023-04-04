fd95b4b412737edaf9031f06028cae2a6a964075
Aries Macandili <aries.macandili@momentummedia.com.au>
2023-04-04 19:40:24 +0800

    feat: Route observables.

--- src/app/users/user/user.component.ts
+++ src/app/users/user/user.component.ts
@@ -1,5 +1,7 @@
-import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
+import { Subscription } from 'rxjs';
+
 import { User } from '../user.model';
 
 @Component({
@@ -7,17 +9,29 @@ import { User } from '../user.model';
     templateUrl: './user.component.html',
     styleUrls: ['./user.component.css']
 })
-export class UserComponent implements OnInit {
+export class UserComponent implements OnInit, OnDestroy {
+    paramsSubscription: Subscription;
     user: User = { id: 1, name: 'test' };
 
     constructor(
         private route: ActivatedRoute
-    ) { }
+    ) {
+        this.paramsSubscription = this.route.params.subscribe(
+            (params: Params) => {
+                this.user.id   = params['id'];
+                this.user.name = params['name'];
+            }
+        );
+    }
 
     ngOnInit() {
         this.user = {
             id    : this.route.snapshot.params['id'],
             name  : this.route.snapshot.params['name']
         };
+    }
+
+    ngOnDestroy() {
+        this.paramsSubscription.unsubscribe();
     }
 }
