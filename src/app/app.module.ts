79daab7fc2ce92e6f013a393cd71bfbb1e4b01eb
Aries Macandili <aries.macandili@momentummedia.com.au>
2023-04-05 22:09:57 +0800

    featL: Route wildcard and redirections.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,7 @@ import { UserComponent } from './users/user/user.component';
 import { EditServerComponent } from './servers/edit-server/edit-server.component';
 import { ServerComponent } from './servers/server/server.component';
 import { ServersService } from './servers/servers.service';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 const appRoutes: Routes = [
     {
@@ -40,6 +41,14 @@ const appRoutes: Routes = [
                 component : UserComponent
             }
         ]
+    },
+    {
+        path       : 'not-found',
+        component  : PageNotFoundComponent
+    },
+    {
+        path       : '**',
+        redirectTo : '/not-found'
     }
 ];
 
@@ -51,7 +60,8 @@ const appRoutes: Routes = [
         ServersComponent,
         UserComponent,
         EditServerComponent,
-        ServerComponent
+        ServerComponent,
+        PageNotFoundComponent
     ],
     imports: [
         BrowserModule,
