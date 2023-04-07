d5c8b183ce0aa81279faf70d0733f7e35b390341
Aries Macandili <aries.macandili@momentummedia.com.au>
2023-04-07 18:32:28 +0800

    feat: Implement resolve guard.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -10,6 +10,7 @@ import { PageNotFoundComponent } from "./page-not-found/page-not-found.component
 import { AuthGuard } from "./auth-guard.service";
 import { CanDeactivateGuard } from "./servers/edit-server/can-deactivate.guard";
 import { ErrorPageComponent } from "./error-page/error-page.component";
+import { ServerResolver } from "./servers/server/server-resolver";
 
 const appRoutes: Routes = [
     {
@@ -24,7 +25,8 @@ const appRoutes: Routes = [
         children         : [
             {
                 path      : ':id',
-                component : ServerComponent
+                component : ServerComponent,
+                resolve   : { server: ServerResolver }
             },
             {
                 path          : ':id/edit',
--- src/app/servers/server/server.component.ts
+++ src/app/servers/server/server.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute, Params, Router } from '@angular/router';
+import { ActivatedRoute, Data, Router } from '@angular/router';
 
 import { ServersService } from '../servers.service';
 import { Server } from '../server.model';
@@ -21,8 +21,12 @@ export class ServerComponent implements OnInit {
     }
 
     ngOnInit() {
-        this.activatedRoute.params.subscribe((params: Params) => {
-            this.server = this.serversService.getServer(+params['id']);
+        // this.activatedRoute.params.subscribe((params: Params) => {
+        //     this.server = this.serversService.getServer(+params['id']);
+        // });
+
+        this.activatedRoute.data.subscribe((data: Data) => {
+            this.server = data['server'];
         });
     }
 
