cc54590b82f7667fd69051fcca4051e08cc2e3f6
Aries Macandili <aries.macandili@momentummedia.com.au>
2023-04-08 17:53:35 +0800

    feat: Creating a custom observable.

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
-import { Subscription, interval } from 'rxjs';
+import { Subscription, Observable, interval, Observer } from 'rxjs';
 
 @Component({
     selector: 'app-home',
@@ -12,9 +12,36 @@ export class HomeComponent implements OnInit, OnDestroy {
     constructor() { }
 
     ngOnInit() {
-        this.firstObsSubscription = interval(1000).subscribe((count) => {
-            console.log(count);
+        // this.firstObsSubscription = interval(1000).subscribe((count) => {
+        //     console.log(count);
+        // });
+        const customIntervalObservable = Observable.create((observer: any) => {
+            let count = 0;
+            setInterval(() => {
+                observer.next(count);
+                if (count === 2) {
+                    observer.complete();
+                }
+                if (count > 3) {
+                    observer.error(new Error('Count is greater 3!'));
+                }
+                count++;
+            }, 1000);
         });
+
+        this.firstObsSubscription = customIntervalObservable.subscribe(
+            (data : any) => {
+                console.log('observer.next() has been called');
+                console.log(data);
+            },
+            (error : any) => {
+                console.log('observer.error() has been called');
+                console.error(error);
+            },
+            () => {
+                console.log('observer.complete() has been called');
+            }
+        );
     }
 
     ngOnDestroy(): void {
