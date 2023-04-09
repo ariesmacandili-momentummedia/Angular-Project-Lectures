e3cec8328574f5100f947435fad50ff677599efe
Aries Macandili <aries.macandili@momentummedia.com.au>
2023-04-09 19:09:16 +0800

    feat: Submitting and accessing form data.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
     selector: 'app-root',
@@ -6,7 +7,14 @@ import { Component } from '@angular/core';
     styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+    @ViewChild('userForm') userForm = {} as NgForm;
+
     suggestUserName() {
         const suggestedName = 'Superuser';
+    }
+
+    onSubmit() {
+        console.log('Submitted!');
+        console.log(this.userForm.value);
     }
 }
