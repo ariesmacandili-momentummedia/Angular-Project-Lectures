a8670cd5996d815afd68e967cb23d5df181f9f9f
Aries Macandili <aries.macandili@momentummedia.com.au>
2023-04-11 22:34:58 +0800

    feat: Arrays of Form Controls (FormArray)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { FormArray, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
     selector: 'app-root',
@@ -16,8 +16,18 @@ export class AppComponent implements OnInit {
                 'username' : new FormControl(null, Validators.required),
                 'email'    : new FormControl(null, [Validators.required, Validators.email]),
                 'gender'   : new FormControl('Male')
-            })
+            }),
+            'hobbies'  : new FormArray([])
         });
+    }
+
+    get hobbyControls() {
+        return (<FormArray>this.signupForm.get('hobbies')).controls;
+    }
+
+    onAddHobby() {
+        const control = new FormControl(null, Validators.required);
+        (<FormArray>this.signupForm.get('hobbies')).push(control);
     }
 
     onSubmit() {
