8200b245d9715760838ae98a4adb57858cecf9fd
Aries Macandili <aries.macandili@momentummedia.com.au>
2023-04-11 22:52:42 +0800

    feat: Creating Custom Validators

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,16 +9,24 @@ import { FormArray, FormControl, FormGroup, Validators } from '@angular/forms';
 export class AppComponent implements OnInit {
     genders = ['Male', 'Female'];
     signupForm = <FormGroup>{};
+    forbiddenUsernames = ['Chris', 'Anna'];
 
     ngOnInit() {
         this.signupForm = new FormGroup({
             'userData' : new FormGroup({
-                'username' : new FormControl(null, Validators.required),
+                'username' : new FormControl(null, [Validators.required, this.forbiddenNames.bind(this)]), // It is mandatory to use bind and add "this".
                 'email'    : new FormControl(null, [Validators.required, Validators.email]),
                 'gender'   : new FormControl('Male')
             }),
             'hobbies'  : new FormArray([])
         });
+    }
+
+    forbiddenNames(control: FormControl): {[s: string]: boolean} | null { // Signature is mandatory.
+        if (this.forbiddenUsernames.indexOf(control.value) !== -1) {
+            return {'nameIsForbidden': true}; // The key is the name of the short error code. Key name can be anything.
+        }
+        return null; // This is fixed as Angular's spec. If null is returned, it means that the validation is passed.
     }
 
     get hobbyControls() {
