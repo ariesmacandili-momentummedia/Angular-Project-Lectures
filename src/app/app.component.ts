1ac47dba591559db1ea26eb7f653a8ba86b622c8
Aries Macandili <aries.macandili@momentummedia.com.au>
2023-04-12 23:24:07 +0800

    feat: Lecture assignment for Reactive Forms.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AbstractControl, FormControl, FormGroup, Validators } from '@angular/forms';
+import { Observable } from 'rxjs';
 
 @Component({
     selector    : 'app-root',
     templateUrl : './app.component.html',
     styleUrls   : ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+    form = <FormGroup>{};
+
+    ngOnInit() {
+        this.form = new FormGroup({
+            'projectName'   : new FormControl(null, Validators.required, this.validateProjectName.bind(this)),
+            'email'         : new FormControl(null, [Validators.required, Validators.email]),
+            'projectStatus' : new FormControl('Stable')
+        });
+    }
+
+    validateProjectName(control: AbstractControl): Promise<any> | Observable<any> {
+        return new Promise((resolve, reject) => {
+            setTimeout(() => {
+                let resolveValue = null;
+                if (control.value === 'Test') {
+                    resolveValue = { 'projectNameIsForbidden' : true };
+                }
+                resolve(resolveValue);
+            }, 1500);
+        });
+    }
+
+    onSubmit() {
+        console.log(this.form.value);
+    }
 }
