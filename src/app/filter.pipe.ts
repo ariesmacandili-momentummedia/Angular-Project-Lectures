7a48fb03be30d726f6ed7f1ab686ed7c04be6e2a
Aries Macandili <aries.macandili@momentummedia.com.au>
2023-04-16 22:47:00 +0800

    feat: Creating a filter pipe.

---
+++ src/app/filter.pipe.ts
@@ -0,0 +1,23 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+    name: 'filter'
+})
+export class FilterPipe implements PipeTransform {
+
+    transform(value: any, ...args: any[]): any {
+        let [filterString, status] = args;
+        if (value.length === 0 || filterString === '') {
+            return value;
+        }
+
+        let resultArray = [];
+        for (let item of value) {
+            if (item[status] === filterString) {
+                resultArray.push(item);
+            }
+        }
+
+        return resultArray;
+    }
+}
