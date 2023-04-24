8f04766da1682043a81a44a0bf1b8fd1b7fec98d
Aries Macandili <aries.macandili@momentummedia.com.au>
2023-04-24 20:54:19 +0800

    feat: Response interceptors.

--- src/app/services/auth-interceptor.service.ts
+++ src/app/services/auth-interceptor.service.ts
@@ -1,4 +1,5 @@
-import { HttpHandler, HttpInterceptor, HttpRequest } from '@angular/common/http';
+import { HttpEventType, HttpHandler, HttpInterceptor, HttpRequest } from '@angular/common/http';
+import { tap } from 'rxjs';
 
 export class AuthInterceptorService implements HttpInterceptor {
   constructor() { }
@@ -8,6 +9,16 @@ export class AuthInterceptorService implements HttpInterceptor {
     const modifiedRequest = request.clone({
         headers : request.headers.append('Auth', 'xyz') // Add a new header named auth.
     });
-    return next.handle(modifiedRequest);
+
+
+    return next
+        .handle(modifiedRequest)
+        .pipe(tap(event => {
+            console.log(event);
+            if (event.type === HttpEventType.Response) {
+                console.log('Response arrived, body data: ');
+                console.log(event.body);
+            }
+        }));
   }
 }
