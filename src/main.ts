cec13b6ead0a0134c207c8b206aca4f7ebba4472
Aries Macandili <aries.macandili@momentummedia.com.au>
2023-05-14 20:32:28 +0800

    feat: Services in Standalone Components

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,8 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
 
-bootstrapApplication(AppComponent);
+bootstrapApplication(AppComponent, {
+    providers: [
+        // insert provider classes here.
+    ]
+});
