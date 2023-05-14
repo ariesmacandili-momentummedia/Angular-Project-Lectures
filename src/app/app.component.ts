92be3cd47d440f63236ac3fa236caf7cb9433194
Aries Macandili <aries.macandili@momentummedia.com.au>
2023-05-14 20:44:49 +0800

    feat: Routing on standalone components.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
 import { WelcomeComponent } from './welcome/welcome.component';
 
 @Component({
     standalone  : true,
     imports    : [
-        WelcomeComponent
+        WelcomeComponent,
+        RouterModule
     ],
     selector    : 'app-root',
     templateUrl : './app.component.html',
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,10 @@
+import { importProvidersFrom } from '@angular/core';
 import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
+import { AppRoutingModule } from './app/app-routing.module';
 
 bootstrapApplication(AppComponent, {
     providers: [
-        // insert provider classes here.
+        importProvidersFrom(AppRoutingModule)
     ]
 });
